92dbafca61602fb27bacb83f944fd4eab913faa9
darek-giza <darek_giza@op.pl>
2019-12-25 00:26:23 +0100

    Centering the login panel

--- src/main/java/pl/dariusz/giza/springbootimageuploader/gui/LoginView.java
+++ src/main/java/pl/dariusz/giza/springbootimageuploader/gui/LoginView.java
@@ -3,34 +3,35 @@ package pl.dariusz.giza.springbootimageuploader.gui;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
-import pl.dariusz.giza.springbootimageuploader.MainView;
 
 @Route("login")
 @Tag("sa-login-view")
 @Theme(value = Lumo.class, variant = Lumo.DARK)
 @PageTitle("Login")
-public class LoginView extends MainView {
+public class LoginView extends VerticalLayout {
+
 
     public LoginView() {
 
 
         VerticalLayout layout = new VerticalLayout();
 
-        LoginForm login = new LoginForm();
+//        layout.getStyle().set("border", "10px dotted DarkOrange");
+        layout.setSizeFull();
 
+        LoginForm loginForm = new LoginForm();
 
+        layout.setHorizontalComponentAlignment(FlexComponent.Alignment.CENTER, loginForm);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 
-        layout.add(login);
-
-        layout.setHorizontalComponentAlignment(Alignment.CENTER,login);
-
-        login.setAction("login");
-        getElement().appendChild(login.getElement());
+        layout.add(loginForm);
+        add(layout);
 
     }
 
